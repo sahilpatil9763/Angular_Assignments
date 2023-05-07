c5686f6ea35784600541a415953ebf938c1b7f02
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2023-05-08 01:39:15 +0530

    Assignment 9

---
+++ Assignment_9/Marvellous/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Marvellous';
+  public value:any;
+
+  public str : string = '';
+  public i: any = 2;
+
+  // public Send(data : any)
+  // {
+  //   this.value = data;
+  // }
+
+  public Check(data : any)
+  {
+    for(data;data>this.i;this.i++)
+    {
+        if(data % this.i==0)
+        {
+            break;
+        }
+    }
+    if(data==this.i)
+    {
+        console.log("Prime Number");
+        this.str = data + " is Prime Number";
+    }
+    else
+    {
+      console.log("Composite Number");
+      this.str = data + " Composite Number";
+    }
+  }
+}
