e2ef590035a81c6c70443670ccb9b8739fede8ea
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2023-05-07 15:36:53 +0530

    .

--- Assignment_8/Marvellous/src/app/app.module.ts
+++ Assignment_8/Marvellous/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
+import { ParentComponent } from './parent/parent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    ParentComponent
   ],
   imports: [
     BrowserModule,
--- Assignment_8/Marvellous/src/app/child/child.component.ts
+++ Assignment_8/Marvellous/src/app/child/child.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  templateUrl: './child.component.html'
 })
 export class ChildComponent {
+  // Create object of event class
+  @Output() public MyEvent = new EventEmitter();
+ 
+  // Action listener for button
+  public SendEvent()
+  {
+    // Send the event to parent
+    this.MyEvent.emit('Message from child');
+  }
 
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
